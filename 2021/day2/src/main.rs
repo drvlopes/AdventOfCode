ad8c0b715e16b3a0c85c44c9765e636b6de6b964
Diogo Lopes <2211850@my.ipleiria.pt>
2023-03-15 11:04:46 +0000

    day2 done

--- 2021/day2/src/main.rs
+++ 2021/day2/src/main.rs
@@ -1,3 +1,75 @@
+use std::{io, fs, ops::{Index}};
+
 fn main() {
-    println!("Hello, world!");
+    loop{
+        println!("[1, 2]");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        
+        let vec = give_vec();
+
+        match guess.trim().parse::<u16>().unwrap() {
+            1 => {part1(vec); break},
+            2 => {part2(vec); break},
+            _ => println!("Invalid input"),
+        }
+    }
+}
+
+fn give_vec() -> Vec<String>{
+    return fs::read_to_string("files/input.txt")
+    .expect("Unable to read file")
+    .lines()
+    .map(|s| s.trim().to_string())
+    .collect();
+}
+
+fn part1(vec: Vec<String>){
+    let mut hp: i32 = 0;
+    let mut d: i32 = 0;
+    let mut num:i32;
+    
+    for x in vec {
+        let i: Vec<&str> = x.split(" ").collect();
+        num = i.index(1).parse::<i32>().unwrap();
+
+        if x.contains("up"){
+            d-= num;
+        }
+        else if x.contains("down") {
+            d += num;
+        }
+        else {
+            hp += num;
+        }
+    }
+
+    println!("{}", d * hp)
+
+}
+
+fn part2(vec: Vec<String>){
+    let mut hp: i32 = 0;
+    let mut d: i32 = 0;
+    let mut aim: i32 = 0;
+    let mut num: i32;
+    
+    for x in vec {
+        let i: Vec<&str> = x.split(" ").collect();
+        num = i.index(1).parse::<i32>().unwrap();
+
+        if x.contains("up"){
+            aim -= num;
+        }
+        else if x.contains("down") {
+            aim += num;
+        }
+        else {
+            hp += num;
+            d += num * aim;
+        }
+    }
+
+    println!("{}", d * hp)
+
 }
---
+++ 2021/day2/target/debug/deps/day2-2874be48704e8a34.d
@@ -0,0 +1,5 @@
+/home/diogo/Documents/projects/AdventOfCode/2021/day2/target/debug/deps/day2-2874be48704e8a34: src/main.rs
+
+/home/diogo/Documents/projects/AdventOfCode/2021/day2/target/debug/deps/day2-2874be48704e8a34.d: src/main.rs
+
+src/main.rs:
